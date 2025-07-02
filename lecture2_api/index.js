1b070a89cdeeba6902a7e87d9d4e5ed2caf558f9
Sanjilsharma7074 <sanjilsharma456@gmail.com>
2025-07-02 10:47:53 +0530

    lecture 2

--- lecture2_api/index.js
+++ lecture2_api/index.js
@@ -2,12 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+
 //This is an API
 app.get('/',(req,res) => {
-  res.send("Server is Live.")
+  // res.send("Server is Live.")
+  const result = {
+    name : "nothing",
+    work : null
+  }
+  res.json(result);//object is passed into res.json either made through a var or directly
+})
+// An API is been hit through your browser
+
+app.post("/",(req,res) => {
+  console.log(req.body);
+  res.send("ok");
 })
 
 app.listen(PORT,()=>{
   console.log(`Server is running on http://localhost:8000`);
-  
 })
